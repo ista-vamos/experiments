44087d4e277f70081d5ec5f9452d8538169ad989
Marek Chalupa <mchqwerty@gmail.com>
2023-10-12 17:59:04 +0000

    Add some sanity checks

--- scripts/run-experiments.sh
+++ scripts/run-experiments.sh
@@ -1,3 +1,31 @@
+# Environment checks
+
+
+echo "-- Checking the environment --"
+
+RUST_LLVM=$(rustc --version --verbose | grep LLVM | cut -d ' ' -f 3)
+CLANG_VER=$(clang -v 2>&1 | grep version | cut -d ' ' -f 4)
+
+if [ "$RUST_LLVM" == "$CLANG_VER" ]; then
+	echo "OK: rustc and clang are compatible"
+else
+	echo "ERROR: rustc and clang are not compatible (rust LLVM: $RUST_LLVM vs. clang LLVM: $CLANG_VER)"
+fi
+
+
+source setup-vars.sh
+
+if [ "$vamos_buffers_BUILD_TYPE" != "Release" ]; then
+	echo "The build type of VAMOS ($vamos_buffers_BUILD_TYPE) is not Release, this slows down the experiments very much."
+else
+	echo "OK: the build type of VAMOS"
+fi
+
+# -----------------
+echo " -- Starting the experiments --"
+sleep 1
+
+
 pushd scalability
 make experiments
 popd
